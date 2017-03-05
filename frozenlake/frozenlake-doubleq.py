9075e4a624e7a49d92e5d56aba3df7227e2493ec
ming li <mingyli34@gmail.com>
2017-03-04 16:38:19 -0800

    frozenlake-v0 success. penalize if falls into hole

--- frozenlake/frozenlake-doubleq.py
+++ frozenlake/frozenlake-doubleq.py
@@ -3,39 +3,42 @@ from gym import wrappers
 import numpy as np
 
 env = gym.make('FrozenLake-v0')
-env = wrappers.Monitor(env, './experiments/frozenlake-v0')
+# env = wrappers.Monitor(env, './experiments/frozenlake-doubleq', force=True)
 
-Q = np.zeros([env.observation_space.n, env.action_space.n])
+Q = (np.zeros([env.observation_space.n, env.action_space.n]),
+     np.zeros([env.observation_space.n, env.action_space.n]))
 #0 left, 1 down, 2 right, 3 up
 
-lr = 0.8
-gamma = 0.9
-num_episodes = 2000
+alpha = 0.8
+gamma = 0.99
+num_episodes = 3000
 
 def train():
-    rewards = []
+    rewards = [0 for _ in range(100)] #moving average over 100 episodes
+    alt = 1
     for i in range(num_episodes):
         obs0 = env.reset()
         ep_reward = 0
-        for t in range(100):
+        for t in range(200):
             # env.render()
-            action = np.argmax(Q[obs0,:] + np.random.randn(1, env.action_space.n) * 1./(i+1))
+            action = np.argmax(Q[alt][obs0,:] + np.random.randn(1, env.action_space.n) * 1./(i+1))
             # action = env.action_space.sample();
             obs1, reward, done, info = env.step(action)
-            Q[obs0, action] = Q[obs0, action] + lr*(reward+gamma*np.max(Q[obs1,:]) - Q[obs0, action])
+            Q[alt][obs0, action] = Q[alt][obs0, action] + alpha*(reward + gamma*np.max(Q[1-alt][obs1,:]) - Q[alt][obs0, action])
             ep_reward += reward
             obs0 = obs1
+            alt = 1 - alt
             if done:
                 break
         rewards.append(ep_reward)
-
-    print("Score over time: " + str(sum(rewards) / num_episodes))
+        rewards.pop(0)
+    print("Average reward over 100 episodes: " + str(sum(rewards) / len(rewards)))
     print("Final Q-Table Values")
-    print(Q)
+    print(Q[alt])
 
 def simulate():
     obs0 = env.reset()
-    for t in range(100):
+    for t in range(200):
         env.render()
         action = np.argmax(Q[obs0,:])
         obs1, reward, done, info = env.step(action)
@@ -47,4 +50,4 @@ def simulate():
 
 if __name__ == '__main__':
     train()
-    simulate()
+    # simulate()
---
+++ frozenlake/frozenlake-v0.py
@@ -0,0 +1,59 @@
+import gym
+from gym import wrappers
+import numpy as np
+
+env = gym.make('FrozenLake-v0')
+env = wrappers.Monitor(env, './experiments/frozenlake-v0', force=True)
+
+Q = np.random.rand(env.observation_space.n, env.action_space.n)
+Q = Q / 10 ** 5
+#0 left, 1 down, 2 right, 3 up
+
+alpha = 0.3
+gamma = .9
+num_episodes = 4000
+
+def train():
+    epsilon = .5
+    rewards = [0 for _ in range(100)] #moving average over 100 episodes
+    for i in range(num_episodes):
+        obs0 = env.reset()
+        ep_reward = 0
+        for t in range(200):
+            # env.render()
+            if np.random.random_sample() < epsilon:
+                action = np.random.randint(0, env.action_space.n)
+            else:
+                action = np.argmax(Q[obs0,:])
+            obs1, reward, done, info = env.step(action)
+            Q[obs0, action] += alpha*(reward + gamma*np.max(Q[obs1,:]) - Q[obs0, action])
+            #penalize if fall into hole
+            if done and reward == 0:
+                Q[obs0, action] -= 1 * 10 ** -2
+            ep_reward += reward
+            obs0 = obs1
+            epsilon -= 10 ** -4
+            if done:
+                break
+        rewards.append(ep_reward)
+        rewards.pop(0)
+
+    print("Average reward over 100 episodes: " + str(sum(rewards) / len(rewards)))
+    print("Final Q-Table Values")
+    print(Q)
+
+def simulate():
+    obs0 = env.reset()
+    for t in range(200):
+        env.render()
+        action = np.argmax(Q[obs0,:])
+        obs1, reward, done, info = env.step(action)
+        obs0 = obs1
+        if done:
+            env.render()
+            break
+    print(obs0, reward)
+
+if __name__ == '__main__':
+    train()
+    # simulate()
